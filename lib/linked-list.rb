fec028c6de9a1a18ccbcd5f21ac7fab1ddff7791
Cathy Rolfs <crolfs47@gmail.com>
2022-12-13 10:36:15 -0600

    Add to string method

--- lib/linked-list.rb
+++ lib/linked-list.rb
@@ -4,21 +4,22 @@ class LinkedList
   attr_reader :head, :tail
   
   def initialize
-    # @linked_list = []
     @head = nil
     @tail = nil
-    append('Node A')
-    prepend('Node B')
-    append('Node C')
-    append('Node D')
-    p "head is #{head.value}, points to #{head.next_node.value}"
-    p "tail is #{tail.value}, points to #{tail.next_node}"
-    p "list length is #{size}"
-    p "node at index 3 is #{at(3).value}"
+    # append('Node A')
+    # prepend('Node B')
+    # append('Node C')
+    # append('Node D')
+    # p "head is #{head.value}, points to #{head.next_node.value}"
+    # p "tail is #{tail.value}, points to #{tail.next_node}"
+    # p "list length is #{size}"
+    # p "node at index 3 is #{at(3).value}"
     # pop
     # p @tail
     # p "new list length is #{size}"
-    p contains?('Node A')
+    # p contains?('Node A')
+    # p find('Node A')
+    # to_s
   end
 
   # adds a new node containing value to the end of the list
@@ -97,10 +98,30 @@ class LinkedList
 
   # returns the index of the node containing value, or nil if not found
   def find(value)
+    return unless contains?(value)
+
+    i = 0
+    length = size
+    node = @head
+    while i < length
+      return i if node.value == value
+
+      node = node.next_node
+      i += 1
+    end
   end
 
   # represent your linked list objects as strings so you can print them out and view them in the console
   def to_s
+    i = 0
+    length = size
+    node = @head
+    while i < length
+      print "#{node.value} -> "
+      node = node.next_node
+      i += 1
+    end
+    puts 'nil'
   end
 
 end
